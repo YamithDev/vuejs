9cd3d47af352c29a9e8df3a3d547972587c606c4
YamidDev <yamidcuetomazo@gmail.com>
2018-10-07 22:31:40 -0500

    transiciones y animaciones
    y preparacion para vue-router

---
+++ transiciones/js/main.js
@@ -0,0 +1,18 @@
+new Vue({
+	el:'main',
+	data:{
+		mostrar:true,
+		mensajes:{
+			transicion: 'Transiciones Css con Vue.js',
+			animacion: 'Animaciones Css con Vue Js',
+			animationCustom: 'Animaciones custom CSS con Vue.js',
+			entreElementos:'Transiciones entre elementos con Vue.js'
+		}
+	},
+	methods:{
+
+	},
+	cumputed:{
+
+	}
+});
